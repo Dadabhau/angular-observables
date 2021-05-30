8f3f98d86b6fea3a43c3ccef473aef8d92306faf
dadabhau <dadabhaut@ithena.ai>
2021-05-30 12:51:41 +0530

    Observable Basic

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { interval, Subscription } from "rxjs";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  constructor() {}
+  private firstObsSubscription: Subscription;
+  ngOnInit() {
+    //Analyzing Angular Observables
+    this.firstObsSubscription = interval(1000).subscribe((count) => {
+      console.log(count);
+    });
+  }
+  ngOnDestroy() {
+    this.firstObsSubscription.unsubscribe();
+  }
+}
