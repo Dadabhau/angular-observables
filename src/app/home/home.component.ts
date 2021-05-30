2af29812161c6f6987b8aeee174597bfe013f930
dadabhau <dadabhaut@ithena.ai>
2021-05-30 20:41:59 +0530

    Errors & Completion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,12 +20,34 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count === 2) {
+          // Errors & (Completion)
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error("Count is greater 3!"));
+        }
         count++;
       }, 1000);
     });
-    customIntervalObservable.subscribe((data) => {
-      console.log(data);
-    });
+
+    // (Errors) & Completion
+    this.firstObsSubscription = customIntervalObservable.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log("Completed!");
+      }
+    );
+
+    // customIntervalObservable.subscribe((data) => {
+    //   console.log(data);
+    // });
   }
   ngOnDestroy() {
     this.firstObsSubscription.unsubscribe();
